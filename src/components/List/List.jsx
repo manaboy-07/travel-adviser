99416eaa8877cdde13b5f33ac607c5b09c7e1257
manaboy-07 <manassehoruebor@gmail.com>
2023-07-25 01:58:38 +0100

    building list component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import {
   CircularProgress,
   Grid,
@@ -11,13 +11,27 @@ import {
   Select,
 } from "@mui/material";
 function List() {
+  const [type, setType] = useState("")
+  const [rating, setRating] = useState("")
   return (
     <div className='container'>
       <Typography variant='h4'>Restaurants</Typography>
       <FormControl className="formControl">
         <InputLabel>Type</InputLabel>
-        <Select value={''} onChange={''}>
-
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+         <MenuItem value="restaurants">Restaurants</MenuItem>
+         <MenuItem value="hotels">Hotels</MenuItem>
+         <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className="formControl">
+        <InputLabel>Rating</InputLabel>
+        <Select value={type} onChange={(e) => setRating(e.target.value)}>
+         <MenuItem value={0}>All</MenuItem>
+         <MenuItem value={3}>Above 3.0</MenuItem>
+         <MenuItem value={4}>Above 4.0</MenuItem>
+         <MenuItem value={4.5}>Above 4.5</MenuItem>
+         
         </Select>
       </FormControl>
     </div>
